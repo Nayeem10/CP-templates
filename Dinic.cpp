ee49346c8923f4b3554b011575ec55a714d09e3b
Nayeem10 <nayeemhossain2110@gmail.com>
2025-11-03 11:40:38 +0600

    Dinic

--- Dinic.cpp
+++ Dinic.cpp
@@ -5,11 +5,10 @@ struct Dinic{
     };
     int n, s, t;
     vector<vector<edge>> g;
-    vector<int> level;
+    vector<int> level, ptr;
     Dinic(int _n) : n(_n + 5){
         g.resize(n);
     }
-    
     void addEdge(int u, int v, LL cap){
         g[u].push_back({v, (int) g[v].size(), 0, cap, 0LL});
         g[v].push_back({u, (int) g[u].size() - 1, 1, 0LL, 0LL});
@@ -31,7 +30,7 @@ struct Dinic{
     LL dfs(int u, LL flow){
         if(u == t) return flow;
         LL ret = 0;
-        for(int i = 0; i < (int) g[u].size() and flow; i++){
+        for(int &i = ptr[u]; i < (int) g[u].size() and flow; i++){
             edge &e = g[u][i];
             int v = e.to;
             if(level[v] != level[u] + 1 or e.cap <= e.flow) continue;
@@ -45,8 +44,54 @@ struct Dinic{
         this->s = s, this->t = t;
         LL flow = 0;
         while(bfs()){
+            ptr.assign(n, 0);
             flow += dfs(s, INF);
         }
         return flow;
+    }
+    set<int> vertexCover(){
+        set<int> cover;
+        vector<pair<int, int>> match;
+        for(int u = 0; u < n; u++){
+            if(u == s or u == t) continue;
+            for(auto e: g[u]){
+                int v = e.to;
+                if(v == s or v == t or e.isRev) continue;
+                if(e.flow == e.cap){
+                    match.push_back({u, v});
+                    cover.insert(u), cover.insert(v);
+                }
+            }
+        }
+        auto check = [&](int u){
+            for(auto e: g[u]){
+                int v = e.to;
+                if(v == s or v == t or cover.find(v) != cover.end()) continue;
+                return 1;
+            }
+            return 0;
+        };
+
+        while(match.size()){
+            vector<pair<int, int>> cur;
+            int m = match.size();
+            for(int i = 0; i < m; i++){
+                auto [u, v] = match[i];
+
+                if(check(u)){
+                    cover.erase(v);
+                }else if(check(v)){
+                    cover.erase(u);
+                }else{
+                    cur.push_back({u, v});
+                }
+            }
+            if((int) cur.size() == m){
+                cover.erase(cur.back().first);
+                cur.pop_back();
+            }
+            match = cur;
+        }
+        return cover;
     }
 };
